8270f29b11898311b6d446e638868474ae57f617
pilha laudosbenitz <laudosbenitz@gmail.com>
2026-02-24 14:19:18 -0300

    configurando servidor para entregar o frontend

--- back/server.js
+++ back/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
@@ -6,11 +7,16 @@ const estoqueRoutes = require('./routes/estoqueRoutes');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../front'))); 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../front/login.html'));
+});
 
 app.use('/auth', authRoutes);
 app.use('/estoque', estoqueRoutes);
 
 const PORT = process.env.PORT || 4000;
+app.use(express.static(path.join(__dirname, '../front')));
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
