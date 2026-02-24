18be77294dd7cf94b116a0b7071652bf37acf21e
pilha laudosbenitz <laudosbenitz@gmail.com>
2026-02-24 15:07:45 -0300

    Ajuste de patch

--- back/server.js
+++ back/server.js
@@ -7,15 +7,16 @@ const estoqueRoutes = require('./routes/estoqueRoutes');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../front'))); 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../front/login.html'));
+app.use(express.static(path.resolve(__dirname, '..', 'front')));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'front', 'login.html'));
 });
 
 app.use('/auth', authRoutes);
 app.use('/estoque', estoqueRoutes);
 
 const PORT = process.env.PORT || 4000;
+app.use(express.static(path.resolve(__dirname, '..', 'front')));
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
